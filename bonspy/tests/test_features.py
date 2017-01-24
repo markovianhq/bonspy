bce7111c2b927290e054dffb765468c41b785947
Georg Walther <georgwalther@gmail.com>
2017-01-24 10:23:02 +0100

    Test that stripping leading www is specific to domain feature

--- bonspy/tests/test_features.py
+++ bonspy/tests/test_features.py
@@ -7,6 +7,12 @@ def test_apply_operations_domain():
     assert value == 'test.com'
 
 
+def test_apply_operations_other_feature():
+    value = _apply_operations('other_feature', 'www.test.com')
+
+    assert value == 'www.test.com'
+
+
 def test_apply_operations_segment():
     value = _apply_operations('segment', 1)
 
