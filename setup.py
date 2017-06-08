9672f0f5cf53ab24ec76cce9dfd20c7965a34406
Alexander Volkmann <alexander.volkmann@markovian.com>
2017-06-08 21:11:57 +0200

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='bonspy',
-    version='1.2.4',
+    version='1.2.5',
     description='Library that converts bidding trees to the AppNexus Bonsai language.',
     author='Alexander Volkmann, Georg Walther',
     author_email='contact@markovian.com',
