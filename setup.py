47273f1c4b2884402162b90cdfe34132431c5a21
Georg Walther <georgwalther@gmail.com>
2017-09-18 20:42:38 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='bonspy',
-    version='1.2.7',
+    version='1.2.8',
     description='Library that converts bidding trees to the AppNexus Bonsai language.',
     author='Alexander Volkmann, Georg Walther',
     author_email='contact@markovian.com',
